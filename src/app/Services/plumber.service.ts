27da792669e2b1d0be991b074f1f7de055b0d49f
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-15 06:06:16 -0500

    crated handler service to handle errors

--- src/app/Services/plumber.service.ts
+++ src/app/Services/plumber.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable } from 'rxjs';
 import { BASE_URL } from "../../environments/environment"
 import { APIResponse } from '../Interfaces/apiResponse';
 import { Plumber } from '../Interfaces/plumber';
+import { HandlerService } from './handler.service';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +12,7 @@ export class PlumberService {
 
   ENDPOINT = BASE_URL+"/plumbers/";
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient, private handler:HandlerService) { }
 
   getAllPlumbers():Observable<APIResponse<Plumber[]>>{
     return this._http.get<APIResponse<Plumber[]>>(this.ENDPOINT);
@@ -26,7 +27,10 @@ export class PlumberService {
   }
 
   updatePlumber(id:string, data:Partial<Plumber>):Observable<APIResponse<Plumber>>{
-    return this._http.patch<APIResponse<Plumber>>(this.ENDPOINT +id, data);
+    return this._http.patch<APIResponse<Plumber>>(this.ENDPOINT +id, data)
+    .pipe(
+      catchError(this.handler.handleError)
+    );
   }
   deletePlumber(id:string):Observable<APIResponse<Plumber>>{
     return this._http.delete<APIResponse<Plumber>>(this.ENDPOINT+id);
