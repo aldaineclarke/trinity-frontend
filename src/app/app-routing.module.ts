7af5c7ec19828d249aad8b9a70f1e7a8536ba3b2
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-15 00:52:50 -0500

    created dashboard and set up admin routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './Pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"admin",
+    component:DashboardComponent,
+    children:[
+      // {
+      //   path:"",
+      //   component: Dashboard
+      // }
+    ]
+  },
+  {
+    path: "",
+    redirectTo:"/admin",
+    pathMatch:"full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
