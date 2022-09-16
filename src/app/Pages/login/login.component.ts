9b44a0bbb4f36a68482a23ef44cbd92a2ede698c
shavvan burrell <shavvanburrell@gmail.com>
2022-09-15 18:40:44 -0700

    sign in form vaildate completed

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -9,13 +9,17 @@ import {FormBuilder, Validators} from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   plumForm = this.fb.group({ 
-    email : ['', Validators.required],
+    email : ['', [Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$" )]  ],
+    // email : ['', [Validators.required,Validators.pattern("/^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$" )]  ],
     password : ['', Validators.required]
   
 
 
 
   })
+  get createControl(){
+    return this.plumForm.controls;
+  }
 
   submit(){
     console.log(this.plumForm.value)
