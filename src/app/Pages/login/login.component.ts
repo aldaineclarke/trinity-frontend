791eb79efc73f47984d1bc11ec7fb0669118270d
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-17 19:53:16 -0500

    fixed up login page

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -6,8 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  plumForm = this.fb.group({ 
+    email : ['', [Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$" )]  ],
+    // email : ['', [Validators.required,Validators.pattern("/^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$" )]  ],
+    password : ['', Validators.required]
+  
 
-  constructor() { }
+
+
+  })
+  get createControl(){
+    return this.plumForm.controls
+  }
+
+  submit(){
+    console.log(this.plumForm.value)
+    
+  }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
